02db9f2eed75bcc4e11b95c3cc7982c2d8e7ccc1
s0uvenir <brian.dunar@gmail.com>
2021-06-28 16:10:48 -0400

    probably wont use any of this, but updated all the packages and tests just in case

--- contracts/mocks/LootSwapPairMock.sol
+++ contracts/mocks/LootSwapPairMock.sol
@@ -2,7 +2,7 @@
 
 pragma solidity 0.6.12;
 
-import "@lootswap/core/contracts/UniswapV2Pair.sol";
+import "@project-monarch/core/contracts/UniswapV2Pair.sol";
 
 contract LootSwapPairMock is UniswapV2Pair {
     constructor() public UniswapV2Pair() {}
